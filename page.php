093af5b3ac0cf23ac3de2561c2f520ebca4808dc
Karl Chvojka <karl.chvojka@gmail.com>
2017-08-22 09:52:00 -0400

    Styling Dashboard

--- page.php
+++ page.php
@@ -1,35 +1,30 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: Home Page
+*/
+get_header(); ?>
 
 <!-- WRAP SITE -->
 <div id="site_wrap">
 
-	<!-- WRAP HEADER -->
-	<div id="header_wrap">
-    </div>
-    <!-- END WRAP HEADER -->
-
     <!-- WRAP CONTENT -->
-    <div id="content_wrap">
-			<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
-				<div class="post">
+		<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
+		<div class="container">
+		  <div class="row">
+		    <div class="col">
 					<h1><?php the_title(); ?></h1>
-					<p><?php the_content(); ?></p>
-				</div>
+  				<p><?php the_content(); ?></p>
+		    </div>
+		    <div class="col">		    </div>
+		  </div>
+		</div>
 
-			<?php endwhile; ?>
 
-			<?php endif; ?>
+		<?php endwhile; ?>
 
-    </div>
+		<?php endif; ?>
 
     <!-- END WRAP CONTENT -->
 
-    <!-- WRAP FOOTER -->
-    <div id="footer_wrap">
-    </div>
-    <!-- END WRAP FOOTER -->
-
-</div>
-<!-- END WRAP SITE -->
 
 <?php get_footer(); ?>
