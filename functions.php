4148f31a425fd2d7f88c450221471ddd39852fc4
Karl Chvojka <karl.chvojka@gmail.com>
2017-08-28 11:36:00 -0400

    Login Page Customization
    Edited, completed login page customization

--- functions.php
+++ functions.php
@@ -63,6 +63,29 @@ function login_redirect( $redirect_to, $request, $user ){
     return home_url('dashboard-2');
 }
 add_filter( 'login_redirect', 'login_redirect', 10, 3 );
+
+/* --------------------------------------- */
+/* -------- CUSTOMIZE LOGIN PAGE --------- */
+/* --------------------------------------- */
+
+function my_custom_login() {
+echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/tlx_login/tlx_login.css" />';
+}
+add_action('login_head', 'my_custom_login');
+
+function my_login_logo_url() {
+return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+return 'Learning Exchange CI Tool';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+/* END LOGIN PAGE FUNCTIONS
+
+
 /* ------------------------------------------------------------
 :: ADD GROUPS EXTENSIONS
 --------------------------------------------------------------- */
